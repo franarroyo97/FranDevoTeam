4ec8e6fae5292bb292faefcc6d2e8b05b971fa7a
Francisco Arroyo Lopez <farroyolo@dragosolutions.com>
2019-03-22 13:25:41 +0100

    beta 2

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,13 @@ router.get('/contacto', function (req, res, next) {
 router.get('/galeria', function (req, res, next) {
   res.render('galeria', { title: 'contacto' });
 });
+/*
+router.get('/api/form', function (req, res, next) {
+  let nombre = req.body.name;
+  let email = req.body.email;
+  res.render('index', {
+    nombre: nombre,
+    email: email,
+  });
+}); */
 module.exports = router;
